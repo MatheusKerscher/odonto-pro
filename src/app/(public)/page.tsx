f3b0e9b801db250c427d0655f5c904da05d8bfa1
MatheusKerscher <kerschermatheus12@gmail.com>
2026-01-12 19:09:13 -0300

    feat: create `call to action` section

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,14 @@
 import Header from "@/components/public/header";
+import Hero from "@/components/public/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <main>Olá, mundo!</main>
+      <main className="container mx-auto mt-20 px-4 xl:px-5 min-h-[100dvh-80px]">
+        <Hero />
+      </main>
     </>
   );
 }
